4b80b9af14aa511d7ace05ad2d04e85f6783be4f
jatinsuneja <s.jatin1903@gmail.com>
2019-01-27 02:17:22 -0800

    code cleanup

--- src/CameraComponent.js
+++ src/CameraComponent.js
@@ -5,33 +5,9 @@ import ReactCamera from './ReactCamera';
 class CameraComponent extends Component {
     render (){
         return (
-            <View>
-                <View>
-                    <ReactCamera/>
-                </View>
-            </View>
+            <ReactCamera/>
         )
     }
 }
 
 export default CameraComponent;
-
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#42f4a1',
-    },
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-    },
-    instructions: {
-      textAlign: 'center',
-      color: '#333333',
-      marginBottom: 5,
-    },
-  });
